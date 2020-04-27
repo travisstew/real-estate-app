d6309f36aca99c45408f26419c6d1a9f2e572f02
Travis <travisstew214@gmail.com>
2020-04-27 01:22:16 -0400

    update

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use('/', require('./routes/api'));
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
 
-    app.get('*', (req,res)=>{
+    app.get('/*', (req,res)=>{
       res.sendFile(path.join(__dirname, 'client','build','index.html'))
     })
 };
